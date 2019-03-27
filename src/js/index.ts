1cb4698c3b0f4e9ae87731d2daeb93de37ee1884
DanMoll <dani948d@edu.easj.dk>
2019-03-27 12:54:36 +0100

    Sat en knap op som printer data ud i html.

--- src/js/index.ts
+++ src/js/index.ts
@@ -5,11 +5,21 @@ import axios,{
 import { ICar } from "./ICar";
 
 let Contentelement: HTMLDivElement = <HTMLDivElement> document.getElementById("content");
+let GetAllCarsButton: HTMLButtonElement = <HTMLButtonElement> document.getElementById("GetAllButton");
 
+GetAllCarsButton.addEventListener('click',showAllCars)
 function showAllCars():void {
     axios.get<ICar[]>("https://webapicar20190326034339.azurewebsites.net/api/cars")
     .then(function (response: AxiosResponse<ICar[]>) : void{
         console.log(response);
+
+        let result: string = "<ol>"
+
+        response.data.forEach((car: ICar) => {
+            result += "<li>" + car.model + " " + car.vendor + "" + car.id + "</li>"
+        });
+        result += "</ol>"
+        Contentelement.innerHTML = result;
     })
     .catch(function(error:AxiosError) : void{
             console.log("Error!");
@@ -17,5 +27,3 @@ function showAllCars():void {
         })
         console.log("Er i slutningen af showAllCars function");
 }
-
-showAllCars();
