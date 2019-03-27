234c9e692ad43dda3682125653e99665722ff02a
DanMoll <dani948d@edu.easj.dk>
2019-03-27 12:26:40 +0100

    Get kommando

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,12 +1,21 @@
-interface Person {
-    firstName: string;
-    lastName: string;
+import axios,{
+    AxiosResponse,
+    AxiosError,
+} from "../../node_modules/axios/index"
+import { ICar } from "./ICar";
+
+let Contentelement: HTMLDivElement = <HTMLDivElement> document.getElementById("content");
+
+function showAllCars():void {
+    axios.get<ICar[]>("https://webapicar20190326034339.azurewebsites.net/api/cars")
+    .then(function (response: AxiosResponse<ICar[]>) : void{
+        console.log(response);
+    })
+    .catch(function(error:AxiosError) : void{
+            console.log("Error!");
+            console.log(error);
+        })
+        console.log("Er i slutningen af showAllCars function");
 }
 
-function greeter(person: Person): string {
-    return "Hello, " + person.firstName + " " + person.lastName;
-}
-let user: Person = { firstName: "John", lastName: "Doe" };
-
-let element: HTMLDivElement = <HTMLDivElement> document.getElementById("content");
-element.innerHTML = greeter(user);
+showAllCars();
